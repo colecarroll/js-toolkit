18f442942b7ce2ca4e6e4c8ca430bf27c66cc879
Cole Carroll <colecarroll2@gmail.com>
2018-01-20 04:29:33 -0700

    created first mongoose model class for Users with googleid property

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
+require("./models/User");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
