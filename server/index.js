c49819cb50f2d1ec2bc4b418dd18a95d6c823314
Cole Carroll <colecarroll2@gmail.com>
2018-01-25 13:28:05 -0700

    trying to deploy heroku

--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(methodOverride("_method"));
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
